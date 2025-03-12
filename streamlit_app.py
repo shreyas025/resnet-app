9c4efdccaf0c69d198ee3b247292d871da6c7fbd
shreyas025 <shreyashwalke25@gmail.com>
2025-03-12 12:11:22 +0530

    stremlit changes 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ if uploaded_file is not None:
     # Send the image to the Flask server
     files = {"file": uploaded_file.getvalue()}
     response = requests.post(
-        "https://resnet-app.onrender.com",
+        "https://resnet-app.onrender.com/predict",
         files=files,
         auth=("admin", "password")
     )
